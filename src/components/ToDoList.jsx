35d9aafcb4e1b92cafe59931419a2c7eb6943d60
krutarthdubey <krutarthdubey2001@gmail.com>
2026-05-18 20:08:44 +0530

    Added ToDoList Components

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,24 @@
+import ToDoItem from "./ToDoItem";
+
+function ToDoList({
+  tasks,
+  deleteTask,
+  toggleComplete,
+  editTask,
+}) {
+  return (
+    <div>
+      {tasks.map((task) => (
+        <ToDoItem
+          key={task.id}
+          task={task}
+          deleteTask={deleteTask}
+          toggleComplete={toggleComplete}
+          editTask={editTask}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ToDoList;
